c1708c5f9115492a93d658d4eb142d1d82072d6c
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-06 14:18:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -28,6 +28,13 @@ end
 introduction_with_language( "Edwin" )
 
 
+#introduction_with_language_optional
+def introduction_with_language(name , language = 'Ruby' )
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language( "Edwin" , "Python")
+
 
 
 
