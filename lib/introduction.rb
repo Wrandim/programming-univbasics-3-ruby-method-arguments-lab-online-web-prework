b2d4378420cbc723fe5a88aa26e7d2f29b99e7e5
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-06 14:03:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,14 +3,22 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 #introduction
-def introduction(name = 'Dan') 
+def introduction(name) 
   puts "Hi, my name is #{name}."
 end
 
-introduction
+introduction("Dan")
+introduction("Josh")
+
 
 #introduction_with_language
-def 
+def introduction_with_language(name , language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language(Dan , Ember.js)
+introduction_with_language(Josh , React)
+
 
 
 
