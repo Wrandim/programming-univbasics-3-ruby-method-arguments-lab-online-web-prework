146aec6cd04605baad93d6386f447904205c8329
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-06 13:50:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,11 +3,12 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 #introduction
-def introduction(name = 'Bill') 
+def introduction(name = 'Dan') 
   puts "Hi, my name is #{name}."
 end
 
-intro
+introduction
+
 
 
 
